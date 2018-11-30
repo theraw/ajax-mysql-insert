c97963b8531c9571c175710a59ecbfe3b42b11f4
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-12-01 00:02:22 +0100

    Update ok.php

--- ok.php
+++ ok.php
@@ -1,28 +1,6 @@
 <?php
-$servername = "127.0.0.1";
-$user = "raw";
-$pass = "goforit";
-$dbname = "tester";
-
-$conn = new mysqli($servername, $user, $pass, $dbname);
-if ($conn->connect_error) {
-    die("DB FAIL : " . $conn->connect_error);
-} 
-
-	$username = $_POST['name'];
-	$password = $_POST['password'];
-    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
-    if ($conn->query($sql) === TRUE) {
-        header('Content-type: application/json');
-        $result = array ('result'=>1);
-        echo json_encode($result);
-    } else {
-        header('Content-type: application/json');
-        $result = array ('result'=>0);
-        echo json_encode($result);
-    }
-
-$conn->close();
+# Helpful in coding part returns what you send so you can understand better then build your code.
+print_r($_POST);
 ?>
 
 
